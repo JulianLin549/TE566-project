3d35cdd165c2cbbdec6bebfc5562b6b304da07aa
JulianLin549 <sres3416@gmail.com>
2022-03-10 03:41:29 -0600

    readme

--- FE/src/components/MainPage/MainPage.tsx
+++ FE/src/components/MainPage/MainPage.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const MainPage = () => {
     return (
         <div>
-            <h1>Welcome to Budget Planning Platform!</h1>
+            <h1>Welcome to Financial Platform!</h1>
 
         </div>
     );
